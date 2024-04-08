32eb2303a2c014ba97f96ce65f01477b64b47dd1
farah-lli <farah.oushen@gmail.com>
2024-04-08 00:21:17 +0000

    fix bar

--- void-dotfiles/cfg/sway/scripts/statusbar.sh
+++ void-dotfiles/cfg/sway/scripts/statusbar.sh
@@ -21,15 +21,15 @@ battery () {
 	elif [ "$bat_status" = "Not charging" ]; then
 		bat_status=""
 	elif [ "$bat_capacity" -gt 80 ]; then
-		bat_status=""
+		bat_status=" "
 	elif [ "$bat_capacity" -gt 60 ]; then
-		bat_status=""
+		bat_status=" "
 	elif [ "$bat_capacity" -gt 40 ]; then
-		bat_status=""
+		bat_status=" "
 	elif [ "$bat_capacity" -gt 20 ]; then
-		bat_status=""
+		bat_status=" "
 	else
-		bat_status=""
+		bat_status=" "
 	fi
 	echo "$bat_status $bat_capacity%"
 }
@@ -54,30 +54,6 @@ volume_pulse() {
 	fi
 
 }
-volume_alsa() {
-
-	mono=$(amixer -M sget Master | grep Mono: | awk '{ print $2 }')
-
-	if [ -z "$mono" ]; then
-		muted=$(amixer -M sget Master | awk 'FNR == 6 { print $7 }' | sed 's/[][]//g')
-		vol=$(amixer -M sget Master | awk 'FNR == 6 { print $5 }' | sed 's/[][]//g; s/%//g')
-	else
-		muted=$(amixer -M sget Master | awk 'FNR == 5 { print $6 }' | sed 's/[][]//g')
-		vol=$(amixer -M sget Master | awk 'FNR == 5 { print $4 }' | sed 's/[][]//g; s/%//g')
-	fi
-
-	if [ "$muted" = "off" ]; then
-		echo " muted"
-	else
-		if [ "$vol" -ge 65 ]; then
-			echo " $vol%"
-		elif [ "$vol" -ge 40 ]; then
-			echo " $vol%"
-		elif [ "$vol" -ge 0 ]; then
-			echo " $vol%"
-		fi
-	fi
-}
 
 #==========================
 # NETWORK
@@ -93,32 +69,11 @@ network() {
 }
 
 #==========================
-# RAM:
-#==========================
-ram() {
-	mem=$(free -h | awk '/Mem:/ { print $3 }' | cut -f1 -d 'i')
-	echo  "$mem"
-}
-
-#==========================
-# CPU:
-#==========================
-cpu() {
-	read -r cpu a b c previdle rest < /proc/stat
-	prevtotal=$((a+b+c+previdle))
-	sleep 0.5
-	read -r cpu a b c idle rest < /proc/stat
-	total=$((a+b+c+idle))
-	cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-	echo  "$cpu"%
-}
-
-#==========================
 # DISPLAY:
 #==========================
 main() {
 	while true; do
-		echo " [ $(network) ] [ $(volume_pulse) ] [ $(battery) ] [ $(clock) ]"
+        echo " [ $(network) ] [ $(volume_pulse) ] [ $(battery) ] [ $(clock) ]"
 		sleep 1
 	done
 }
