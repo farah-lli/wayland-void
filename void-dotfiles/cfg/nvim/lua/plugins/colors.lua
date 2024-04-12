e9999002f107d22edb8f4bd14c97fe6c0285187d
farah-lli <farah.oushen@gmail.com>
2024-04-12 11:13:21 +0000

    added gruvbox

--- void-dotfiles/cfg/nvim/lua/plugins/colors.lua
+++ void-dotfiles/cfg/nvim/lua/plugins/colors.lua
@@ -1,19 +1,29 @@
 -----------------------------------------------------------
 -- Colorscheme
 -----------------------------------------------------------
--- For Moonbow Theme
--- vim.cmd([[colorscheme moonbow]]) -- Set color scheme
-
--- For Gruvbox Material Theme
+require("gruvbox").setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = true,
+})
 vim.o.background = "dark" -- or "light" for light mode
-vim.cmd("let g:gruvbox_material_background= 'hard'")
-vim.cmd("let g:gruvbox_material_transparent_background=2")
-vim.cmd("let g:gruvbox_material_diagnostic_line_highlight=1")
-vim.cmd("let g:gruvbox_material_diagnostic_virtual_text='colored'")
-vim.cmd("let g:gruvbox_material_enable_bold=1")
-vim.cmd("let g:gruvbox_material_enable_italic=1")
-vim.cmd([[colorscheme gruvbox-material]]) -- Set color scheme
--- changing bg and border colors
-vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
-vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "Normal" })
-vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
+vim.cmd("colorscheme gruvbox")
--- void-dotfiles/cfg/nvim/lua/plugins/lazy.lua
+++ void-dotfiles/cfg/nvim/lua/plugins/lazy.lua
@@ -3,56 +3,57 @@
 -----------------------------------------------------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-    { "nvim-lua/plenary.nvim" },
-    { "nvim-tree/nvim-web-devicons" },
-    { "goolord/alpha-nvim", event = "VimEnter" },
-    { "stevearc/dressing.nvim", event = "VeryLazy" },
-    { "ggandor/leap.nvim" },
-    { "akinsho/toggleterm.nvim" },
-    { "sainnhe/gruvbox-material" },
-    { "yamatsum/nvim-cursorline" },
-    { "arturgoms/moonbow.nvim" },
-    { "nvimdev/galaxyline.nvim" },
-    { "nvim-tree/nvim-tree.lua" },
-    { "stevearc/oil.nvim" },
-    { "nvim-telescope/telescope.nvim" },
-    { "akinsho/bufferline.nvim" },
-    { "lukas-reineke/indent-blankline.nvim" },
-    { "echasnovski/mini.indentscope" },
-    { "tpope/vim-surround" },
-    { "tpope/vim-commentary" },
-    { "tpope/vim-fugitive" },
-    { "tpope/vim-repeat" },
-    { "lewis6991/gitsigns.nvim" },
-    { "norcalli/nvim-colorizer.lua" },
-    { "windwp/nvim-autopairs" },
-    { "windwp/nvim-ts-autotag" },
-    { "nvim-treesitter/nvim-treesitter" },
-    -- LSP
-    { "neovim/nvim-lspconfig" },
-    { "glepnir/lspsaga.nvim" },
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
-    -- CMP
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/cmp-buffer" },
-    { "hrsh7th/cmp-path" },
-    { "hrsh7th/cmp-cmdline" },
-    { "hrsh7th/nvim-cmp" },
-    -- SNP
-    { "L3MON4D3/LuaSnip" },             -- snippet engine
-    { "saadparwaiz1/cmp_luasnip" },     -- for autocompletion
-    { "rafamadriz/friendly-snippets" }, -- useful snippets
-    { "onsails/lspkind.nvim" },         -- vs-code like pictograms
+	{ "nvim-lua/plenary.nvim" },
+	{ "nvim-tree/nvim-web-devicons" },
+	{ "goolord/alpha-nvim", event = "VimEnter" },
+	{ "stevearc/dressing.nvim", event = "VeryLazy" },
+	{ "ggandor/leap.nvim" },
+	{ "akinsho/toggleterm.nvim" },
+	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+	{ "yamatsum/nvim-cursorline" },
+	{ "arturgoms/moonbow.nvim" },
+	{ "nvimdev/galaxyline.nvim" },
+	{ "nvim-tree/nvim-tree.lua" },
+	{ "stevearc/oil.nvim" },
+	{ "nvim-telescope/telescope.nvim" },
+	{ "akinsho/bufferline.nvim" },
+	{ "lukas-reineke/indent-blankline.nvim" },
+	{ "echasnovski/mini.indentscope" },
+	{ "tpope/vim-surround" },
+	{ "tpope/vim-commentary" },
+	{ "tpope/vim-fugitive" },
+	{ "tpope/vim-repeat" },
+	{ "lewis6991/gitsigns.nvim" },
+	{ "norcalli/nvim-colorizer.lua" },
+	{ "windwp/nvim-autopairs" },
+	{ "windwp/nvim-ts-autotag" },
+	{ "nvim-treesitter/nvim-treesitter" },
+	-- LSP
+	{ "neovim/nvim-lspconfig" },
+	{ "glepnir/lspsaga.nvim" },
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	-- CMP
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-cmdline" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "stevearc/conform.nvim" },
+	-- SNP
+	{ "L3MON4D3/LuaSnip" }, -- snippet engine
+	{ "saadparwaiz1/cmp_luasnip" }, -- for autocompletion
+	{ "rafamadriz/friendly-snippets" }, -- useful snippets
+	{ "onsails/lspkind.nvim" }, -- vs-code like pictograms
 })
