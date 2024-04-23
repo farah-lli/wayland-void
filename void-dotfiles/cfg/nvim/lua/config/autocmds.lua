481e4fd876a4381e314064c16c2b3d95726e0b67
farah-lli <farah.oushen@gmail.com>
2024-04-23 20:19:25 +0100

    Move Completly to LazyVim

--- void-dotfiles/cfg/nvim/lua/config/autocmds.lua
+++ void-dotfiles/cfg/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here---@diagnostic disable: param-type-mismatch
+------------------------------------------------
+-- Disable autoformat for some buffers:
+------------------------------------------------
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "bash", "sh" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
---
+++ void-dotfiles/cfg/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------
+-- Completions
+-----------------------------------------------------------
+local cmp = require("cmp")
+return {
+  "hrsh7th/nvim-cmp",
+  opts = {
+    mapping = cmp.mapping.preset.insert({
+      ["<C-k>"] = cmp.mapping.select_prev_item(),
+      ["<C-j>"] = cmp.mapping.select_next_item(),
+    }),
+  },
+}
