4d40275b66a59da14bbdaae9409e6b0a00da095a
farah-lli <farah.oushen@gmail.com>
2024-04-23 19:50:40 +0100

    move to LazyVim

---
+++ void-dotfiles/cfg/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,7 @@
+return {
+  "akinsho/bufferline.nvim",
+  keys = {
+    { "<TAB>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    { "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+  },
+}
---
+++ void-dotfiles/cfg/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    pickers = {
+      find_files = {
+        theme = "dropdown",
+      },
+      oldfiles = {
+        theme = "dropdown",
+      },
+      registers = {
+        theme = "dropdown",
+      },
+      colorscheme = {
+        theme = "dropdown",
+      },
+      diagnostics = {
+        theme = "dropdown",
+      },
+    },
+  },
+}
