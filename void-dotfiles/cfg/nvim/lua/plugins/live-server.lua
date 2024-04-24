6ba34bb7dd5cd0a8ddd5138019923b5098f6c150
farah-lli <farah.oushen@gmail.com>
2024-04-24 22:55:19 +0100

    added live-server

---
+++ void-dotfiles/cfg/nvim/lua/plugins/live-server.lua
@@ -0,0 +1,9 @@
+-----------------------------------------------------------
+-- Live Server
+-----------------------------------------------------------
+return {
+  "barrett-ruth/live-server.nvim",
+  build = "npm install -g live-server",
+  cmd = { "LiveServerStart", "LiveServerStop" },
+  config = true,
+}
