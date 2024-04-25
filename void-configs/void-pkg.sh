3e7554c4a40d46e5df83c42eb0320a925fe1d0ab
farah-lli <farah.oushen@gmail.com>
2024-04-25 02:07:04 +0100

    make dwl defaults Compositor

--- void-configs/void-pkg.sh
+++ void-configs/void-pkg.sh
@@ -13,7 +13,7 @@ sudo xbps-install -Sy \
     # COMPOSITOR:
     wayland wayland-protocols xorg-server-xwayland wlroots wlroots-devel \
     # COMPOSITOR-TOOLS:
-    river waylock Waybar swayidle lswt kanshi wlr-randr wvkbd wbg fnott \
+    lswt kanshi wlr-randr wvkbd wbg fnott \
     # WAYLAND QT APP:
     qt5ct qt5-wayland qt6-wayland \
     # SCREENSHOOT TOOLS:
