20cb50d2a95634145cbe6e1b4210f0b77a0fcd1d
farah-lli <farah.oushen@gmail.com>
2024-05-01 10:58:21 +0100

    Fix waybar

--- void-dotfiles/cfg/nvim/lua/plugins/cmp.lua
+++ void-dotfiles/cfg/nvim/lua/plugins/cmp.lua
@@ -1,6 +1,11 @@
 -----------------------------------------------------------
 -- Completions
 -----------------------------------------------------------
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 local cmp = require("cmp")
 return {
   "hrsh7th/nvim-cmp",
@@ -8,28 +13,6 @@ return {
     mapping = cmp.mapping.preset.insert({
       ["<C-k>"] = cmp.mapping.select_prev_item(),
       ["<C-j>"] = cmp.mapping.select_next_item(),
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-          -- they way you will only jump inside the snippet region
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
     }),
   },
 }
