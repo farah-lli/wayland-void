a230fc47c25cdfada16fe1ba8a3b23478a134177
farah-lli <farah.oushen@gmail.com>
2024-05-02 22:10:48 +0100

    added Plugins

--- void-dotfiles/cfg/nvim/lua/plugins/bufferline.lua
+++ void-dotfiles/cfg/nvim/lua/plugins/bufferline.lua
@@ -7,4 +7,9 @@ return {
     { "<TAB>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
     { "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
   },
+  opts = {
+    options = {
+      separator_style = "slant", -- | "thick" | "thin" | { 'any', 'any' },
+    },
+  },
 }
