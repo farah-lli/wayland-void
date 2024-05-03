5686bd5a8d8517cd6c1151b9608387f398c47827
farah-lli <farah.oushen@gmail.com>
2024-05-03 14:03:46 +0100

    The Last Change

--- void-dotfiles/cfg/nvim/lua/plugins/disabled.lua
+++ void-dotfiles/cfg/nvim/lua/plugins/disabled.lua
@@ -6,8 +6,4 @@ return {
   { "folke/tokyonight.nvim", enabled = false },
   -- disable catppuccin (theme)
   { "catppuccin/nvim", enabled = false },
-  -- disbale manage global and project-local settings
-  { "folke/neoconf.nvim", enabled = false },
-  -- disable nvim-spectre (find/replace in multiple files)
-  { "nvim-pack/nvim-spectre", enabled = false },
 }
--- void-dotfiles/cfg/nvim/lua/plugins/indent-blankline.lua
+++ void-dotfiles/cfg/nvim/lua/plugins/indent-blankline.lua
@@ -11,7 +11,7 @@ function M.config()
   vim.opt.list = true
   vim.opt.listchars:append("eol:⤸")
   vim.opt.listchars:append("tab:│ ")
-  -- vim.opt.listchars:append("space:⋅")
+  vim.opt.listchars:append("space:⋅")
 
   require("ibl").setup({
     indent = { char = "│" },
