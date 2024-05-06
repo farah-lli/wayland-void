c8d87961de5332db15ab16e7a85f6a3c923a99d8
farah-lli <farah.oushen@gmail.com>
2024-05-06 21:12:46 +0100

    Settings Oil

--- void-dotfiles/cfg/nvim/lua/plugins/oil.lua
+++ void-dotfiles/cfg/nvim/lua/plugins/oil.lua
@@ -9,15 +9,45 @@ local M = {
     { "SirZenith/oil-vcs-status" },
   },
   keys = {
-    { "<leader>o", "<cmd>Oil<cr>", desc = "Oil" },
+    { "<leader>o", "<cmd>Oil --float<cr>", desc = "Oil" },
     { "<leader>fo", "<cmd>Oil<cr>", desc = "Oil" },
   },
 }
 
 function M.config()
   require("oil").setup({
+    default_file_explorer = true,
+    border = "rounded",
     win_options = {
       signcolumn = "yes:2",
+    },
+    float = {
+      -- Padding around the floating window
+      padding = 2,
+      max_width = 0,
+      max_height = 0,
+      border = "rounded",
+      win_options = {
+        winblend = 0,
+      },
+      progress = {
+        max_width = 0.9,
+        min_width = { 40, 0.4 },
+        width = nil,
+        max_height = { 10, 0.9 },
+        min_height = { 5, 0.1 },
+        height = nil,
+        border = "rounded",
+        minimized_border = "none",
+        win_options = {
+          winblend = 0,
+        },
+      },
+      -- This is the config that will be passed to nvim_open_win.
+      -- Change values here to customize the layout
+      override = function(conf)
+        return conf
+      end,
     },
     keymaps = {
       ["g?"] = "actions.show_help",
