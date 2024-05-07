46f9fc1fb678a3b7c97a6f7ad20ee44b28b1d617
farah-lli <farah.oushen@gmail.com>
2024-05-07 21:28:18 +0100

    nice format

--- void-dotfiles/cfg/nvim/lua/plugins/bigfile.lua
+++ void-dotfiles/cfg/nvim/lua/plugins/bigfile.lua
@@ -8,7 +8,4 @@ return {
   opts = {
     filesize = 2, -- size of the file in MiB, the plugin round file sizes to the closest MiB
   },
-  config = function(_, opts)
-    require("bigfile").setup(opts)
-  end,
 }
