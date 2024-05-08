7bbf8b79fcd9368e5a8d4601305317da9853871c
farah-lli <farah.oushen@gmail.com>
2024-05-08 11:44:15 +0100

    added extra plugs

--- void-dotfiles/cfg/nvim/lua/plugins/indent-blankline.lua
+++ void-dotfiles/cfg/nvim/lua/plugins/indent-blankline.lua
@@ -1,6 +1,3 @@
------------------------------------------------------------
--- Indent Line
------------------------------------------------------------
 local M = {
   "lukas-reineke/indent-blankline.nvim",
   lazy = false,
@@ -10,8 +7,8 @@ function M.config()
   -- set symbols for space and newline
   vim.opt.list = true
   vim.opt.listchars:append("eol:⤸")
-  vim.opt.listchars:append("tab:│ ")
   vim.opt.listchars:append("space:⋅")
+  vim.opt.listchars:append("tab:│ ")
 
   require("ibl").setup({
     indent = { char = "│" },
