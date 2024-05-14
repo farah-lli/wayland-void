6678409d981c405f61e32b5ae5772dcca59bb203
farah-lli <farah.oushen@gmail.com>
2024-05-14 20:53:56 +0100

    change icons

--- void-dotfiles/cfg/nvim/lua/plugins/cmp.lua
+++ void-dotfiles/cfg/nvim/lua/plugins/cmp.lua
@@ -5,6 +5,9 @@ local cmp = require("cmp")
 return {
   "hrsh7th/nvim-cmp",
   opts = {
+    completion = {
+      autocomplete = false,
+    },
     mapping = cmp.mapping.preset.insert({
       ["<C-k>"] = cmp.mapping.select_prev_item(),
       ["<C-j>"] = cmp.mapping.select_next_item(),
